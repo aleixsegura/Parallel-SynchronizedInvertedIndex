b5829628dbc37066139da943fabb3cdb6a4f9a1d
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2024-04-29 23:36:22 +0200

    removed: authors ID data

--- src/Location.java
+++ src/Location.java
@@ -2,8 +2,8 @@
 Práctica 3.
 Código fuente: Location.java
 Grau Informàtica
-48056540H - Aleix Segura Paz.
-21161168H - Aniol Serrano Ortega.
+Aleix Segura Paz.
+Aniol Serrano Ortega.
 --------------------------------------------------------------- */
 public class Location{
     private final Long fileId;
